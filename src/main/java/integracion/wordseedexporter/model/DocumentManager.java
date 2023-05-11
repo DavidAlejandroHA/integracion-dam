b79606d772286d1db86a3505a0ca099dbe15af92
DavidAlejandroHA <99730787+DavidAlejandroHA@users.noreply.github.com>
2023-05-11 14:15:44 +0100

    Test de cambios

--- src/main/java/integracion/wordseedexporter/model/DocumentManager.java
+++ src/main/java/integracion/wordseedexporter/model/DocumentManager.java
@@ -1,9 +1,10 @@
 package integracion.wordseedexporter.model;
 
 import java.io.File;
-import java.io.IOException;
 
-import org.apache.poi.poifs.filesystem.FileMagic;
+import org.odftoolkit.odfdom.doc.OdfTextDocument;
+import org.odftoolkit.odfdom.incubator.search.TextNavigation;
+import org.odftoolkit.odfdom.incubator.search.TextSelection;
 
 public class DocumentManager {
 	
@@ -12,7 +13,7 @@ public class DocumentManager {
 	}
 	
 	public void giveDocument(File f) {
-		try {
+		/*try {
 			System.out.println("a");
 			System.out.println(FileMagic.valueOf(f));
 			System.out.println("b");
@@ -20,7 +21,31 @@ public class DocumentManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println("sassdds");
+		}*/
+		try {
+			//https://odftoolkit.org/api/odfdom/org/odftoolkit/odfdom/doc/OdfTextDocument.html
+			//https://odftoolkit.org/api/odfdom/org/odftoolkit/odfdom/doc/package-summary.html // Tipos de documentos
+			OdfTextDocument textdoc=(OdfTextDocument) OdfTextDocument.loadDocument(f);
+			
+			TextNavigation search1;
+
+			search1= new TextNavigation("por",textdoc);
+
+			while (search1.hasNext()) {
+
+				TextSelection item1 = (TextSelection) search1.getCurrentItem();
+
+				System.out.println(item1);
+
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		//https://docx4java.org/docx4j/Docx4j_GettingStarted.pdf
+		//https://stackoverflow.com/questions/72405729/how-to-programatically-modify-libre-office-odt-document-in-java
+		//https://angelozerr.wordpress.com/2012/12/06/how-to-convert-docxodt-to-pdfhtml-with-java/
+		//https://stackoverflow.com/questions/61805246/docx4j-docx-to-pdf-conversion-docx-content-not-appearing-page-by-page-to-pdf
 		
 	}
 
